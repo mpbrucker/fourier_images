3484286bb45a773ee6b7002d83c3b5996c8276e6
Matthew Brucker <mbrucker@olin.edu>
2017-11-27 16:25:53 -0500

    Made filtering work.

--- FFT2D.m
+++ FFT2D.m
@@ -1,13 +1,19 @@
 img1 = rgb2gray(imread('image1.jpg'));
 img_fft = fftshift(fft2(img1, 512, 512));
-img_mags = log(abs(img_fft) + 1);
-noisy_peaks = ~imregionalmax(img_mags);
-noisy_peaks(250:260,250:260) = 1;
-filt_fft = img_fft.*noisy_peaks;
+
+kern = [0,0,2; 0,2,4; 2,4,4];
+kernel = zeros(size(img_fft));
+kernel(111:113,111:113) = kern;
+kernel_fft = fftshift(fft2(kernel));
+filt_fft = img_fft.*kernel_fft;
+
+% noisy_peaks = ~imregionalmax(img_mags);
+% noisy_peaks(250:260,250:260) = 1;
+% filt_fft = img_fft.*noisy_peaks;
 filt_img = ifft2(ifftshift(filt_fft));
 
 
-
+img_mags = log(abs(img_fft) + 1);
 plot_fft = mat2gray(img_mags);
 plot_filt = mat2gray(log(abs(filt_fft)+1));
 clf;
